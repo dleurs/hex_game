143b1706a12b03c13d7445178f302c68455a48b1
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-04-02 17:02:54 +0200

    Setting up stream user

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:beamer/beamer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hex_game/generated/l10n.dart';
 import 'package:hex_game/navigation/hex_location.dart';
 import 'package:hex_game/ui/screens/page_not_found_screen.dart';
+import 'package:provider/provider.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
@@ -27,21 +29,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: "Hex Game",
-      routerDelegate: routerDelegate,
-      routeInformationParser: BeamerRouteInformationParser(),
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+    return MultiProvider(
+      providers: [
+        StreamProvider<User?>(
+          create: (BuildContext context) =>
+              FirebaseAuth.instance.authStateChanges(),
+          initialData: null,
+        )
       ],
-      supportedLocales: S.delegate.supportedLocales,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      child: MaterialApp.router(
+        title: "Hex Game",
+        routerDelegate: routerDelegate,
+        routeInformationParser: BeamerRouteInformationParser(),
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:beamer/beamer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hex_game/generated/l10n.dart';
 import 'package:hex_game/ui/components/main_scaffold.dart';
 import 'package:hex_game/ui/screens/players_screen.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static final BeamPage beamLocation = BeamPage(
@@ -22,24 +24,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return MainScaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                S.of(context).hex_game_title,
-                style: Theme.of(context).textTheme.headline3,
-              ),
-              Text(
-                Intl.getCurrentLocale(),
-              ),
-              Text(
-                "Hello World",
-              ),
-            ],
+      body: Consumer<User?>(builder: (context, user, child) {
+        return Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  S.of(context).hex_game_title,
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                Text(
+                  Intl.getCurrentLocale(),
+                ),
+                Text(
+                  user?.isAnonymous.toString() ?? "User not connected",
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
